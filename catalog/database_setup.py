7175e2ce175c7b57dff788932e3dd2f81a65458f
Wonju Jeon <wjjeon@theworkshop.com>
2016-02-16 21:07:29 +0900

    First commit
    Basic structure established
    Main view function implemented
    category item show, new, edit delete functions implemented

---
+++ catalog/database_setup.py
@@ -0,0 +1,74 @@
+import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    modified_date = Column(DateTime, default=datetime.datetime.utcnow)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'modified_date': self.modified_date,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    modified_date = Column(DateTime, default=datetime.datetime.utcnow)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'modified_date': self.modified_date,
+            'name': self.name,
+            'user_id': self.user_id
+        }
+
+
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
+
+    id = Column(Integer, primary_key=True)
+    modified_date = Column(DateTime, default=datetime.datetime.utcnow)
+    title = Column(String(80), nullable=False)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'modified_date': self.modified_date,
+            'title': self.title,
+            'description': self.description,
+            'category_id': self.category_id,
+            'user_id': self.user_id
+        }
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
