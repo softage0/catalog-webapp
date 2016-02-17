fd212b8720008928c3812e28873ecce7984b4c0b
Wonju Jeon <wjjeon@theworkshop.com>
2016-02-17 22:53:00 +0900

    flask-sslify installed https to http redirection fix

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask_sslify import SSLify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -8,21 +10,19 @@ from config import config
 # Flask initialized
 app = Flask(__name__)
 
+# Force 'url_for' return https URL on Heroku
+if 'DYNO' in os.environ: # only trigger SSLify if the app is running on Heroku
+    sslify = SSLify(app)
+
 # DB session initialized
 engine = create_engine(config.DB_URL)
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
 # It must be imported after app, session declared
 import views
 import json_endpoints
 import account
 
-
 app.secret_key = 'super_secret_key'
-app.debug = True
-app.config.update(dict(
-    PREFERRED_URL_SCHEME='https'
-))
